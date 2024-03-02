12ade9ac3a16249399fc6a008e1468af8e9a99ea
dinhnguyen92 <harrynguyen92@zohomail.com>
2024-03-02 12:30:49 -0700

    Set number of workers to 3

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,4 @@
 # Gunicorn configuration file
-import multiprocessing
-import math
 
 max_requests = 1000
 max_requests_jitter = 50
@@ -12,7 +10,7 @@ bind = "0.0.0.0:8000"
 worker_class = "uvicorn.workers.UvicornWorker"
 
 # Gunicorn and uvicorn hoad huge amounts of memory
-# so instead of using (cpu * 2 + 1) as the max number of workers,
-# we'll just cpu / 3 to avoid out-of-memory errors,
+# so instead of using the recommended (cpu * 2 + 1) as the max number of workers,
+# we'll just 3 (to generate 3 completions in parallel) to avoid out-of-memory errors,
 # especially when we need to reserve memory to load the models.
-workers = math.ceil(multiprocessing.cpu_count() / 3)
+workers = 3
